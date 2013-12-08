f484c2aba09444bb52d93830f4ca25b1544c3c92
Erik Ralston <erik.ralston@gmail.com>
2013-12-07 23:12:07 -0800

    Updated NuGet package

--- InviteOnly/Properties/AssemblyInfo.cs
+++ InviteOnly/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("InviteOnly")]
-[assembly: AssemblyDescription("A simple module for managing invites")]
+[assembly: AssemblyDescription("A simple module for managing invites in any ASP.Net MVC app, offering a simple Action Filter that handles redirecting uninvited users to other actions. See GitHub repo (Project Site) for more details.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Erik Ralston")]
 [assembly: AssemblyProduct("InviteOnly")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright © 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
